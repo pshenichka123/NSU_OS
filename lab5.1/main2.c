500ef47d7decccbbf4c4560908dc11b5240b9d50
pshenichka123 <kolosskv07@gmail.com>
2025-04-28 09:40:02 +0700

    fixed 5.1

--- lab5.1/main2.c
+++ lab5.1/main2.c
@@ -3,8 +3,26 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-
+#define ERROR -1
+#define CHILD 0
 int global_var = 10;
+
+
+
+
+void Child_Process()
+{
+
+    printf("\nChild process - PID: %d\n", getpid());
+    printf("Child process - Parent PID: %d\n", getppid());
+
+    printf("Child process - global_var: address=%p, value=%d\n", (void*)&global_var, global_var);
+    printf("Child process - local_var: address=%p, value=%d\n", (void*)&local_var, local_var);
+
+    printf("Child process - changed global_var: value=%d\n", global_var);
+    printf("Child process - changed local_var: value=%d\n", local_var);
+
+}
 
 int main() {
     int local_var = 20;
@@ -16,48 +34,37 @@ int main() {
 
     pid_t pid = fork();
 
-    if (pid == -1) {
+    if (pid == ERROR) {
         perror("fork failed");
         return 0;;
     }
-
-    if (pid == 0) {
-        printf("\nChild process - PID: %d\n", getpid());
-        printf("Child process - Parent PID: %d\n", getppid());
-
-        printf("Child process - global_var: address=%p, value=%d\n", (void*)&global_var, global_var);
-        printf("Child process - local_var: address=%p, value=%d\n", (void*)&local_var, local_var);
-
-        global_var = 100;
-        local_var = 200;
-        printf("Child process - changed global_var: value=%d\n", global_var);
-        printf("Child process - changed local_var: value=%d\n", local_var);
-
-        _exit(5); заменить на _exit
+    if (pid == CHILD) {
+        Child_Process();
+        _exit(5);
     }
 
     sleep(1);
-
     printf("\nParent process - global_var after child changes: value=%d\n", global_var);
     printf("Parent process - local_var after child changes: value=%d\n", local_var);
 
     int status;
     pid_t child_pid = wait(&status);
 
-    if (child_pid == -1) {
+    if (child_pid == ERROR) {
         perror("wait failed");
         exit(1);
     }
 
-    if (WIFEXITED(status)) {
+    bool executed_orrectly = WIFEXITED(status);
+    if (executed_correctly) {
         printf("Child process %d exited with status: %d\n", child_pid, WEXITSTATUS(status));
     }
-    if (WIFSIGNALED(status)) {
+
+    bool executed_by_signal = WIFSIGNALED(status);
+    if (executed_by_signal) {
         printf("Child process %d was killed by signal: %d\n", child_pid, WTERMSIG(status));
     }
-    else {
-        printf("Child process %d terminated abnormally\n", child_pid);
-    }
+
 
 
     return 0;
