16ba1ce6bee7b46f42b824eb5f4f318cf17498da
pshenichka123 <kolosskv07@gmail.com>
2025-09-25 11:16:29 +0700

    looxmaxxing

--- 3course/lab1.1/f.c
+++ 3course/lab1.1/f.c
@@ -5,7 +5,8 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <unistd.h>
-
+#define SUCCESS 0
+#define THREAD_COUNT 5
 void *mythread(void *arg)
 {
     printf("mythread [%d %d %d]: Hello from mythread!\n", getpid(), getppid(), gettid());
@@ -14,15 +15,15 @@ void *mythread(void *arg)
 
 int main()
 {
-    pthread_t tid[5];
+    pthread_t tid[THREAD_COUNT];
     int err;
 
     printf("main [%d %d %d]: Hello from main!\n", getpid(), getppid(), gettid());
 
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < THREAD_COUNT; i++)
     {
         err = pthread_create(&tid[i], NULL, mythread, NULL);
-        if (err)
+        if (err != SUCCESS)
         {
             fprintf(stderr, "main: pthread_create() failed: %s\n", strerror(err));
             pthread_exit(NULL);
